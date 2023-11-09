d05733253464f28999d4ee26fb29488e1323a9d5
franco fariña <francoemanuel.1994@gmail.com>
2023-11-08 21:14:00 -0300

    primer commit javaScript

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let pers;
+const CON_BEBIDA = 1500;
+const CON_DJ = 1000;
+const CON_TORTA = 1000;
+const MENU1 = 2500;
+const MENU2 = 3000;
+const MENU3 = 3500;
+let nomnbre;
+
+nombre = prompt("Bienvenido aqui va a poder diseñar su evento, ingrese su nombre")
+let evento = prompt("ingrese que tipo de evento va a realizar")
+pers = parseInt(prompt("cuantas personas asistiran al " + evento + "?"))
+
+
+
