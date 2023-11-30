9ed28a959a230483fe678b53f0f7d815329433c8
franco fariña <francoemanuel.1994@gmail.com>
2023-11-29 23:38:46 -0300

    ultima modificacion

--- js/index.js
+++ js/index.js
@@ -8,9 +8,24 @@ const MENU3 = 3500;
 let nomnbre;
 let totalPerCapi = 0;
 
-nombre = prompt("Bienvenido aqui va a poder diseñar su evento, ingrese su nombre");
-let evento = prompt("ingrese que tipo de evento va a realizar");
-pers = parseInt(prompt("cuantas personas asistiran al " + evento + "?"));
+
+class Cliente {
+        constructor (nombre, edad, email ){
+        this.nombre = nombre,
+        this.edad = edad,
+        this.email = email
+    }
+}
+
+nombreIngresado = prompt("Bienvenido aqui va a poder diseñar su evento, ingrese su nombre")
+edadIngresada = prompt("Ingrese su edad")
+emailIngresado = prompt("Ingrese su email")
+const CLIENTE1 = new Cliente (nombreIngresado, edadIngresada, emailIngresado )
+
+
+let evento = prompt("que tipo de evento realizara?")
+
+pers = prompt("cuantas personas asistiran al " + evento + "?")
 
 let platoElegido = prompt("elija su menu: pasta, pescado, asado").toLowerCase();
 switch(platoElegido){
@@ -73,7 +88,7 @@ function costoTotal(gastoPers, personas){
 }
 
 let total = costoTotal ( totalPerCapi, pers)
-alert(" su " + evento + " para " + pers + " personas " + "tiene un valor aproximado de " + totalPerCapi + " por invitado, con un valor total de $ " + total + " muchas gracias " + nombre)
+alert(" su " + evento + " para " + pers + " personas " + "tiene un valor aproximado de " + totalPerCapi + " por invitado, con un valor total de $ " + total + " muchas gracias " + nombreIngresado)
 
 function descuentoEfectivo (precioTotal, descuento){
     let desc = (precioTotal * descuento)/100
@@ -83,3 +98,30 @@ function descuentoEfectivo (precioTotal, descuento){
 
 let pagoEnEfectivo = descuentoEfectivo (total,15)
 alert("realizando el pago en efectivo tiene un 15% de descuento, siendo asi el valor total: $ " + pagoEnEfectivo)
+
+
+class Producto{
+    constructor (articulo, precio, img){
+        this.articulo = articulo,
+        this.precio = precio,
+        this.img = img
+    }
+}
+
+const EVENTO = new Producto (`tu ${evento}`, valorTotal, img)
+const GACEBOS = new Producto ("Gacebos", 15000, "gaceboimg")
+const BARRA_COCTEL = new Producto ("Barra De Tragos", 20000, "barraimg")
+
+const PRODUCTOS = [EVENTO, GACEBOS, BARRA_COCTEL]
+
+todosProductos.forEach(producto => {
+    let div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+                    <p>articulo: ${Producto.articulo}</p>
+                    <p>precio: ${Producto.precio}</p>
+                    <img src= "${Producto.img}"/>
+                    <button>Agregar al carrito</button>
+    `
+    contenerdoProductos.appendChild(div);
+})
